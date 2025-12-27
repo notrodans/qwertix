ef2ac91c8fb8279aa7542af4d3c27ff4d85abd0a
Dmytro Zhukov <notrodans@gmail.com>
2025-12-27 22:17:40 +0200

    Refactor(backend): Replace MSW with real backend

--- apps/frontend/src/app/main.tsx
+++ apps/frontend/src/app/main.tsx
@@ -5,22 +5,10 @@ import { queryClient } from '@/shared/api/query/client';
 import './index.css';
 import App from './App';
 
-async function enableMocking() {
-	if (
-		import.meta.env.DEV &&
-		!(window as unknown as { __SKIP_MSW__: boolean }).__SKIP_MSW__
-	) {
-		const { worker } = await import('@/shared/api/mocks/browser');
-		return worker.start();
-	}
-}
-
-enableMocking().then(() => {
-	createRoot(document.getElementById('root')!).render(
-		<StrictMode>
-			<QueryClientProvider client={queryClient}>
-				<App />
-			</QueryClientProvider>
-		</StrictMode>,
-	);
-});
+createRoot(document.getElementById('root')!).render(
+	<StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
+	</StrictMode>,
+);
