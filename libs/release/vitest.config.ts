1219bfa464178d48b3121c6ebafa41af3ec54b1c
notrodans <notrodans@gmail.com>
2026-01-31 06:02:01 +0200

    feat(ci): enhance CI/CD workflows and transition to SemVer (#3)

---
+++ libs/release/vitest.config.ts
@@ -0,0 +1,15 @@
+import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	plugins: [tsconfigPaths()],
+	test: {
+		globals: true,
+		environment: 'node',
+		include: ['src/**/*.spec.ts'],
+		alias: {
+			bun: path.resolve(__dirname, './src/__mocks__/bun.ts'),
+		},
+	},
+});
